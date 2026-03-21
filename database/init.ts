1db02cb9641124327efcfd66195d47a9560cbd0d
elinapihlajaoja <elinaakatariina@gmail.com>
2026-03-21 16:32:33 +0200

    Yksinkertaistettu liikehistoria ja jaettu käyttäjätiedot eri tauluihin

--- database/init.ts
+++ database/init.ts
@@ -1,54 +1,52 @@
 import { database } from './database'
-
+ 
 /* 
   Tietokannan skeema ja luonti
   Taulut omissa funktioissaan ja luonti initDatabase()-funktiossa.
 */
-
+ 
 export const initDatabase = async () => {
   try {
     // Tätä tarvii, jotta ON DELETE CASCADE toimii
     await database.execAsync(`PRAGMA foreign_keys = ON;`);
-
+ 
     await createExerciseTable();
     await createWorkoutTable();
     await createWorkoutExerciseTable();
-    await createWorkoutSessionTable();
-    await createWorkoutHistoryTable();
-    await createUserInfoTable();
+    await createExerciseLogTable();
+    await createUserProfileTable();
+    await createUserDailyTable();
     await createNutritionTable();
-
+ 
     console.log('Tietokanta luotu.')
   } catch (error) {
     console.error('Tietokannan luominen epäonnistui: ', error)
   }
 }
-
+ 
 const createExerciseTable = async () => {
   await database.execAsync(`
     CREATE TABLE IF NOT EXISTS exercise (
-      id TEXT PRIMARY KEY NOT NULL,
-      user_id TEXT NOT NULL,
+      id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
       name TEXT NOT NULL,
       category TEXT NOT NULL
     );
   `)
 }
-
+ 
 const createWorkoutTable = async () => {
   await database.execAsync(`
     CREATE TABLE IF NOT EXISTS workout (
       id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
       name TEXT NOT NULL,
-      user_id TEXT
     );
   `)
 }
-
+ 
 const createWorkoutExerciseTable = async () => {
   await database.execAsync(`
     CREATE TABLE IF NOT EXISTS workout_exercise (
-      exercise_id TEXT NOT NULL,
+      exercise_id INTEGER NOT NULL,
       workout_id INTEGER NOT NULL,
       PRIMARY KEY(exercise_id, workout_id),
       FOREIGN KEY(exercise_id) REFERENCES exercise(id) ON DELETE CASCADE,
@@ -56,58 +54,52 @@ const createWorkoutExerciseTable = async () => {
     );
   `)
 }
-
-const createWorkoutSessionTable = async () => {
+ 
+const createExerciseLogTable = async () => {
   await database.execAsync(`
-    CREATE TABLE IF NOT EXISTS workout_session (
+    CREATE TABLE IF NOT EXISTS exercise_log (
       id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+      workout_id INTEGER NOT NULL,
+      exercise_id TEXT NOT NULL,
       date INTEGER NOT NULL,
-      workout_id INTEGER,
-      user_id TEXT NOT NULL,
-      FOREIGN KEY(workout_id) REFERENCES workout(id) ON DELETE CASCADE
-    );
-  `)
-}
-
-const createWorkoutHistoryTable = async () => {
-  await database.execAsync(`
-    CREATE TABLE IF NOT EXISTS workout_history (
-      id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
-      timestamp INTEGER NOT NULL,
-      exercise_id TEXT,
       weight REAL,
       reps INTEGER,
-      session_id INTEGER,
-      time REAL,
-      user_id TEXT NOT NULL,
-      FOREIGN KEY(exercise_id) REFERENCES exercise(id) ON DELETE SET NULL,
-      FOREIGN KEY(session_id) REFERENCES workout_session(id) ON DELETE CASCADE
+      set_number INTEGER,
+      FOREIGN KEY(workout_id) REFERENCES workout(id) ON DELETE CASCADE,
+      FOREIGN KEY(exercise_id) REFERENCES exercise(id) ON DELETE CASCADE
     );
   `)
 }
-
-const createUserInfoTable = async () => {
-  await database.execAsync(`
-    CREATE TABLE IF NOT EXISTS user_info (
-      id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
-      user_id TEXT NOT NULL,
-      weight TEXT NOT NULL,
+ 
+const createUserProfileTable = async () => {
+  await database.execAsync(
+    `CREATE TABLE IF NOT EXISTS user_profile (
+      singleton INTEGER PRIMARY KEY CHECK (singleton = 1),
       height REAL,
-      time INTEGER,
-      daily_steps INTEGER,
       steps_goal INTEGER,
       calories_goal INTEGER
     );
   `)
 }
 
+ 
+const createUserDailyTable = async () => {
+  await database.execAsync(`
+    CREATE TABLE IF NOT EXISTS user_daily (
+      id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+      weight REAL,
+      daily_steps INTEGER,
+      date INTEGER NOT NULL UNIQUE
+    );
+  `)
+}
+ 
 const createNutritionTable = async () => {
   await database.execAsync(`
     CREATE TABLE IF NOT EXISTS nutrition (
       id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
-      user_id TEXT NOT NULL,
       name TEXT,
-      time INTEGER NOT NULL,
+      date INTEGER NOT NULL,
       calories INTEGER,
       protein REAL,
       carbs REAL,
