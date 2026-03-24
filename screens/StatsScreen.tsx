508fe05a5aec93d922dc83d74f93000fa8748968
elinapihlajaoja <elinaakatariina@gmail.com>
2026-03-24 18:21:26 +0200

    Lisätty ylävalitsin, tehty valmiiksi pohjat tilastosivun eri näkymille

--- screens/StatsScreen.tsx
+++
@@ -1,9 +0,0 @@
-import { View, Text } from "react-native";
-
-export default function StatsScreen() {
-  return (
-    <View>
-      <Text>Stats Screen</Text>
-    </View>
-  );
-}
---
+++ screens/stats/StatsProgressScreen.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+export default function StatsWeightScreen() {
+  return (
+    <View>
+      <Text>Kehityssivu</Text>
+    </View>
+  );
+}
