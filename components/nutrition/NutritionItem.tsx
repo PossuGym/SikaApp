d8a091a7da5cb0daea108b724a5628e0daf7483b
Zenkku <senja.paakkinen@gmail.com>
2026-03-25 13:59:13 +0200

    Ravintosivu aloitettu

---
+++ components/nutrition/NutritionItem.tsx
@@ -0,0 +1,40 @@
+import { Card, IconButton } from 'react-native-paper';
+import { Nutrition } from '../../types/types';
+
+interface Props {
+  item: Nutrition;
+  onClick: (nutrition: Nutrition) => void;
+  onDelete?: (id: number) => void;
+}
+
+/**
+ * Yksittäisen liikkeen kortti poistonapilla.
+ * Näyttää liikkeen nimen ja kategorian.
+ * @param item ```Nutrition``` tyypin objekti
+ * @param onDelete Välittää liikkeen id:n poistolle(Valinnainen).
+ * @param onClick Välittää koko objektin painallustoiminnalle.
+ * @example <NutrtitionItem item={ex} onClick={handleEdit} onDelete={handleDelete} />
+ * <NutritionItem item={ex} onClick={handleSelect} />
+ */
+export const NutritionItem = ({ item, onClick, onDelete }: Props) => {
+  return (
+    <Card 
+      mode="outlined"
+      onPress={() => onClick(item)}
+    >
+      <Card.Title
+        title={item.name}
+        right={(props) => onDelete ? ( // Poistonappi näytetään vain, jos sen toiminta välitetään propsina
+          <IconButton 
+            {...props} 
+            icon="delete-outline" 
+            iconColor="#B00020" 
+            onPress={() => {
+                if (item.id != null) onDelete?.(item.id);
+            }}
+          />
+        ) : null }
+      />
+    </Card>
+  );
+};
--- screens/NutritionScreen.tsx
+++ screens/NutritionScreen.tsx
@@ -1,9 +1,71 @@
-import { View, Text } from "react-native";
+import { FlatList, StyleSheet, View, Text } from 'react-native';
+import { FAB, Surface } from 'react-native-paper';
+import { useNutrition } from '../hooks/UseNutrition';
+import { NutritionItem } from '../components/nutrition/NutritionItem';
+import { NutritionDialog } from '../components/nutrition/NutritionDialog';
 
-export default function NutrionScreen() {
+/*
+* UI tiedosto pysyy hyvin siistinä, kun kaikki toimintalogiikka/funktiot on eristetty muualle. 
+*/
+export default function NutritionScreen() {
+  const { 
+    nutrition, 
+    isDialogVisible, 
+    selectedNutrition, 
+    openCreateDialog, 
+    openEditDialog, 
+    closeDialog, 
+    saveNutrition,
+    deleteMeal
+  } = useNutrition();
+
   return (
-    <View>
-      <Text>Nutrion Screen</Text>
-    </View>
+    <Surface style={styles.container} elevation={0}>
+
+      {/* Lista liikkeistä, painamalla muokkausdialogi, ikonista poisto*/}
+      <FlatList
+        data={nutrition}
+        renderItem={({ item }) => (
+          <NutritionItem 
+            item={item} 
+            onClick={() => openEditDialog(item)}
+            onDelete={(id) => deleteMeal(id)}
+          />
+        )}
+        ItemSeparatorComponent={() => <View style={{ height: 10 }} />} // Rako itemien väliin
+      />
+
+      {/* Floating Action Button, avaa uuden liikkeen luonnin dialogin */}
+      <FAB
+        icon="plus"
+        customSize={64}
+        style={styles.fab} // FABIN SIJAINTI, JÄÄ DEFAULTTINA NAVIGAATION ALLE
+        onPress={openCreateDialog}
+      />
+
+      {/* Dialogi ExerciseDialog-komponentilla */}
+      <NutritionDialog 
+        visible={isDialogVisible}
+        data={selectedNutrition}
+        onSave={saveNutrition}
+        onDismiss={closeDialog}
+      />
+    </Surface>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 16
+  },
+  item: {
+    marginBottom: 10
+  },
+  fab: {
+    position: 'absolute',
+    margin: 16,
+    right: 10,
+    bottom: 130
+  },
+});
