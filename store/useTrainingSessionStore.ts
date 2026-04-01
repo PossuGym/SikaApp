bbb557ad61d2d022fe3c7b81dedc2af88b0ad144
elinapihlajaoja <elinaakatariina@gmail.com>
2026-04-01 16:51:30 +0300

    lisätty zustand ja storepohjat

---
+++ store/useTrainingSessionStore.ts
@@ -0,0 +1,13 @@
+import { Workout } from "../types/types"
+
+
+export const useTrainingSessionStore = () => {
+  // placeholder funktio
+  const handleOpen = (workout: Workout) => {
+
+  }
+
+  return {
+    handleOpen
+  }
+}
