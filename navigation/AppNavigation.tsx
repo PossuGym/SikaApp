e0d7ecac9d9c5cfbbdcd7677bd57d9184df46b60
veliok <veli.okkonen@gmail.com>
2026-04-04 13:48:36 +0300

    Kokonainen navigaatio stackeilla. Closes #13

--- navigation/AppNavigation.tsx
+++ navigation/AppNavigation.tsx
@@ -3,15 +3,12 @@ import { ActivityIndicator, View } from "react-native";
 import type { RootStackParamList } from "./types";
 import TabNavigation from "./TabNavigation";
 import { useAuth } from "../hooks/useAuth";
-import ProfileScreen from "../screens/ProfileScreen";
 import Auth from '../screens/Auth';
-import SessionScreen from '../screens/session/SessionScreen';
-
 
 /*
-  Navigaatioreitit, jotka eivät ole bottom tabseissa. Esim profiili, kirjautumissivu, yms.
+  Kirjautumisen ja rekisteröinnin navigaatiologiikka.
+  Ennen kirjautumista näytetään Auth.tsx screen.
 */
-
 const Stack = createNativeStackNavigator<RootStackParamList>();
 
 export default function AppNavigation() {
@@ -25,21 +22,13 @@ export default function AppNavigation() {
     );
   }
 
-  /*
-    Jos kirjautunut sisään, reitteinä on tabsit ja profiili.
-    Ei kirjautuneena pelkkä login sivu.
-  */
   return (
     <Stack.Navigator screenOptions={{ headerShown: false }}>
       {claims ? (
-        <>
-          <Stack.Screen name="Tabs" component={TabNavigation} />
-          <Stack.Screen name="Profile" component={ProfileScreen} />
-          <Stack.Screen name="TrainingSession" component={SessionScreen} />
-        </>
+        <Stack.Screen name="Tabs" component={TabNavigation} />
       ) : (
         <Stack.Screen name="Login" component={Auth} />
-      )} 
+      )}
     </Stack.Navigator>
   );
 }
