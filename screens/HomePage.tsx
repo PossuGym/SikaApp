edb98c6a1813e4222db895c443aaf358761fa61e
elinapihlajaoja <elinaakatariina@gmail.com>
2026-04-10 19:13:16 +0300

    Uloskirjautumispainike siirretty profiilisivulle

--- screens/HomePage.tsx
+++ screens/HomePage.tsx
@@ -1,19 +1,12 @@
-import * as React from "react";
-import { View, useWindowDimensions } from "react-native";
-import { Button, Text, Card } from "react-native-paper";
+import { View } from "react-native";
+import { Text, Card } from "react-native-paper";
 import Timer from "../components/homePage/Timer";
-import { SafeAreaView } from "react-native";
-import { useAuth } from "../hooks/useAuth";
 import { FavoriteCard } from "../components/homePage/FavoriteCard";
 import { useWorkout } from "../store/useWorkoutStore";
 
-
-
 export function Homepage() {
-  const { handleSignOut, authLoading } = useAuth();
   const { workouts, exercises, toggleFavorite } = useWorkout();
   const favorites = workouts.filter((w) => w.favorite === 1);
-
 
   return (
     <View style={{ padding: 16, gap: 12, }}>
@@ -39,18 +32,9 @@ export function Homepage() {
         </View>
       )}
 
-      
-
-      <Button mode="outlined" onPress={handleSignOut} loading={authLoading} disabled={authLoading}>
-        Sign out
-      </Button>
-
       <Timer>
-
       </Timer>
     
-
-
     </View>
   );
 }
